83d0175094e15c9b3a28776585058320d3af6c74
mohamadalabsi <absimoda1@gmail.com>
2024-11-18 23:57:06 +0100

    organizing the modules and starting Kapitel 6

--- AlgDat/Kapitel 6/singelLinkedList.py
+++ AlgDat/Kapitel 6/singelLinkedList.py
@@ -16,6 +16,31 @@ class LinkedList:
         while last.next:
             last = last.next
         last.next = new_node
+    def insert_at_beginning (self, data):
+        new_node = Node(data)
+        new_node.next=self.head
+        self.head=new_node
+    def insert_at_position(self , pos, data):
+        new_node = Node(data)
+        temp=self.head
+        for i in range (pos-1):
+            temp=temp.next
+        new_node.next=temp.next
+        temp.next=new_node    
+
+
+    def delete_at_beginning (self):
+        temp=self.head   
+        self.head = temp.next
+        temp.next=None
+    def delete_at_end(self):
+        temp=self.head.next
+        prev=self.head
+        while temp.next is not None:
+            temp=temp.next
+            prev=prev.next
+        prev.next=None     
+            
 
     def delete_node(self, key):
         temp = self.head
@@ -44,6 +69,10 @@ ll = LinkedList()
 ll.insert_at_end(1)
 ll.insert_at_end(2)
 ll.insert_at_end(3)
+ll.insert_at_beginning(55)
+ll.insert_at_position(2,33)
 ll.print_list()
-ll.delete_node(2)
+ll.delete_at_beginning()
+ll.delete_node(33)
+ll.delete_at_end()
 ll.print_list()
---
+++ AlgDat/Kapitel 6/stack.py
@@ -0,0 +1,11 @@
+from pprint import  pprint as pp
+
+stack=[]
+stack.append(1)
+stack.append(2)
+stack.append(3)
+stack.append(4)
+
+stack.pop()
+pp(stack[-1])
+pp(stack)
