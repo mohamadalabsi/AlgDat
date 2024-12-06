da46b429da3ca429856c2ee9c1d48bbdf3895c46
mohamadalabsi <absimoda1@gmail.com>
2024-12-06 23:34:01 +0100

    another small changes

--- AlgDat/SortAlgo/InsertionSort.py
+++ AlgDat/SortAlgo/InsertionSort.py
@@ -3,13 +3,13 @@ def InsertionSort(A:list):
   for j in range(len(A)):
     Key=A[j]
     i=j-1
-    while i>=0 and A[i]<Key :
+    while i>=0 and A[i]>Key :
       A[i+1]=A[i]
       i-=1
       A[i+1]=Key
   return A
 
 
-A=[3,7,4,1,0,8,45]    
+A=[7,3,4,1,0,8,45]    
 result=InsertionSort(A)
 print(result)
